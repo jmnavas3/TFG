65b8da4868f9ed35a7df67eb8f1b53a4c3d6bf10
jmnavas3 <joselillo.sk8@gmail.com>
2024-08-01 15:59:42 +0200

    basic routes added

--- app/backend/routes/api.py
+++ app/backend/routes/api.py
@@ -1,8 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, current_app
+from backend.database.database import Database
 
 
 api_route = Blueprint('api', __name__)
+readiness_route = Blueprint('readiness', __name__)
 
 @api_route.route('', methods=['GET'])
 def index():
     return "Hola Mundo!", 200
+
+@readiness_route.route('', methods=['GET'])
+def db_check():
+    try:
+        db = Database(current_app.config['SQLALCHEMY_DATABASE_URI'])
+        with Database.session_factory(db=db)() as session:
+            if session.is_active:
+                return "OK", 200
+            return "Active", 200
+    except Exception as e:
+        print(str(e))
+        return "KO", 500
