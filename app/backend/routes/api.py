93e7cc027361fbb585ed096a641d9be2271cf9de
jmnavastr <jmnavas@trevenque.es>
2024-08-09 17:38:09 +0200

    enable disable rule controller added

--- app/backend/routes/api.py
+++ app/backend/routes/api.py
@@ -1,24 +1,11 @@
-from flask import Blueprint, current_app
-from app.backend.database.database import Database
-
+from flask import Blueprint
+from app.backend.src.rules.infrastructure.controllers.enable_disable_rule_controller import enable_disable
 
 api_route = Blueprint('api', __name__)
-readiness_route = Blueprint('readiness', __name__)
+api_route.register_blueprint(enable_disable)
+
 
 @api_route.route('', methods=['GET'])
 def index():
     print("hola mundo!")
     return "Hola Mundo!", 200
-
-@readiness_route.route('', methods=['GET'])
-def db_check():
-    try:
-        db = Database(current_app.config['SQLALCHEMY_DATABASE_URI'])
-        with Database.session_factory(db=db)() as session:
-            if session.is_active:
-                print("sesion activa")
-                return "OK", 200
-            return "Active", 200
-    except Exception as e:
-        print(str(e))
-        return "KO", 500
