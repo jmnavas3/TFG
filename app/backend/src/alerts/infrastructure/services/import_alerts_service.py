c619c9ccff1f867032470fe09334a49675594c0d
jmnavastr <jmnavas@trevenque.es>
2024-09-01 22:56:09 +0200

    alerts module uncommited changes

--- app/backend/src/alerts/infrastructure/services/import_alerts_service.py
+++ app/backend/src/alerts/infrastructure/services/import_alerts_service.py
@@ -1,11 +1,11 @@
 from sqlalchemy import Engine
 
 from app.backend.database.models.fast import Fast as EntityModel
-from app.backend.src.alerts.domain.contracts.import_csv_contract import ImportCsv
+from app.backend.src.base.domain.contracts.import_csv_contract import ImportCsv
 from app.backend.src.base.infrastructure.adapters.pandas_adapter import PandasAdapter
 
 
-class ImportCsvService(ImportCsv):
+class ImportAlertsService(ImportCsv):
     _entity_model = EntityModel.__tablename__
 
     def __init__(self, engine: Engine, csv: str = ""):
